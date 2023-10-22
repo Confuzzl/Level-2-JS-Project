89a080b6e12a500b3e7bdf11a2141d9c25a872d5
Confuzzl <fuzzltheduzzl@gmail.com>
2023-10-21 23:04:46 -0400

    maintenance

--- docs/script.js
+++ docs/script.js
@@ -1,24 +1,33 @@
-cards = [];
-for (let i = 0; i < 8; i++) {
-    for (let j = 1; j <= 2; j++) {
-        let cardWrapper = document.createElement("div");
-        cardWrapper.classList = "card_item_wrapper";
-        cardWrapper.id = `${String.fromCharCode(65 + i)}${j}`;
-        cardWrapper.setAttribute("down", "");
-        cardWrapper.onclick = () => {
-            flipCard(cardWrapper);
-        };
-        let card = document.createElement("div");
-        card.classList = "card_item";
+class Card {
+    constructor(element) {
+        this.element = element;
+        this.match = undefined;
+    }
 
-        let face = document.createElement("div");
-        face.classList = "card_face";
-        let back = document.createElement("div");
-        back.classList = "card_back";
-        card.appendChild(face);
-        card.appendChild(back);
-        cardWrapper.appendChild(card);
-        cards.push(cardWrapper);
+    static createPair(name) {
+        let pair = [];
+        for (let j = 0; j < 2; j++) {
+            let card = new Card(document.createElement("div"));
+            let cardDiv = card.element;
+            cardDiv.classList = "card_item";
+            cardDiv.id = `${name}${j}`;
+            setCardClick(card, true);
+            cardDiv.toggleAttribute("down");
+            let cardContent = document.createElement("div");
+            cardContent.classList = "card_content";
+
+            let face = document.createElement("div");
+            face.classList = "card_face";
+            let back = document.createElement("div");
+            back.classList = "card_back";
+            cardContent.appendChild(face);
+            cardContent.appendChild(back);
+            cardDiv.appendChild(cardContent);
+            pair.push(card);
+        }
+        pair[0].match = pair[1];
+        pair[1].match = pair[0];
+        return pair;
     }
 }
 
@@ -28,41 +37,105 @@ function shuffle() {
         [cards[i], cards[j]] = [cards[j], cards[i]];
     }
 }
-shuffle();
 
-cardDisplay = document.getElementById("card_display");
+let cards;
+let cardDisplay;
+let selected;
+let currentIndex;
+function init() {
+    cards = [];
+    for (let i = 0; i < 8; i++) {
+        for (const card of Card.createPair(String.fromCharCode(65 + i))) {
+            cards.push(card);
+        }
+    }
+    shuffle();
 
-function flipCard(cardWrapper) {
-    let current, toggled;
-    if (cardWrapper.getAttribute("down") != null)
-        (current = "down"), (toggled = "up");
-    else if (cardWrapper.getAttribute("up") != null)
-        (current = "up"), (toggled = "down");
+    cardDisplay = document.getElementById("card_display");
+    populateDisplay();
 
-    cardWrapper.removeAttribute(current);
-    cardWrapper.setAttribute("animated", "");
-    setCardClick(cardWrapper, false);
-    setTimeout(() => {
-        setCardClick(cardWrapper, true);
-        cardWrapper.removeAttribute("animated");
-        cardWrapper.setAttribute(toggled, "");
-    }, 1000);
+    selected = [null, null];
+    currentIndex = 0;
 }
 
-function setCardClick(cardWrapper, bool) {
-    if (bool)
-        cardWrapper.onclick = () => {
-            flipCard(cardWrapper);
+function flipCard(card) {
+    let cardDiv = card.element;
+    let current, toggled;
+    if (cardDiv.getAttribute("down") != null)
+        (current = "down"), (toggled = "up");
+    else if (cardDiv.getAttribute("up") != null)
+        (current = "up"), (toggled = "down");
+
+    cardDiv.removeAttribute(current);
+    cardDiv.toggleAttribute("animated");
+    setCardClick(card, false);
+    setTimeout(() => {
+        cardDiv.toggleAttribute("animated");
+    }, 1000);
+    cardDiv.setAttribute(toggled, "");
+}
+
+function setCardClick(card, bool) {
+    let cardDiv = card.element;
+    if (bool) {
+        cardDiv.onclick = () => {
+            selectCard(card);
         };
-    else cardWrapper.onclick = "";
+        cardDiv.toggleAttribute("clickable", true);
+    } else {
+        cardDiv.onclick = "";
+        cardDiv.toggleAttribute("clickable", false);
+    }
+}
+
+function disableCard(card) {
+    let cardDiv = card.element;
 }
 
 function populateDisplay() {
     for (const card of cards) {
-        card.getElementsByClassName("card_face")[0].innerHTML = card.id;
-        cardDisplay.appendChild(card);
+        const cardDiv = card.element;
+        cardDiv.getElementsByClassName(
+            "card_face"
+        )[0].innerHTML = `${cardDiv.id} FACE`;
+        cardDiv.getElementsByClassName(
+            "card_back"
+        )[0].innerHTML = `${cardDiv.id} BACK`;
+        cardDisplay.appendChild(cardDiv);
     }
 }
-populateDisplay();
 
-selected = [null, null];
+function selectCard(card) {
+    if (selected[currentIndex] === null) selected[currentIndex] = card;
+
+    flipCard(card);
+
+    if (++currentIndex < 2) return;
+
+    cards.forEach((card) => {
+        setCardClick(card, false);
+    });
+
+    if (selected[1].match === selected[0]) {
+        console.log("MATCH");
+        cards = cards.filter((item) => {
+            return !selected.includes(item);
+        });
+        console.log(cards.length);
+        cards.forEach((card) => {
+            setCardClick(card, true);
+        });
+        selected = [null, null];
+        currentIndex = 0;
+    } else {
+        setTimeout(() => {
+            flipCard(selected[0]);
+            flipCard(selected[1]);
+            selected = [null, null];
+            currentIndex = 0;
+            cards.forEach((card) => {
+                setCardClick(card, true);
+            });
+        }, 1250);
+    }
+}
